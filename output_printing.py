c010c9a37291db487a9914b528960d549ced6aa7
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-20 23:47:46 +0530

    output and print in python

---
+++ output_printing.py
@@ -0,0 +1,9 @@
+# Printing or outputting using print function/method
+print("Hello World")
+# print numbers
+print(4.5)
+# separated by commas
+print(4.5, "hello")
+# collection of printing with other data types
+print("Hello", 'end', 87, False, end='\n')
+print("Thoufiq - the Best One")
