c245e21ff0665f52acb54b36297cdfc9a9226826
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-21 23:57:59 +0530

    arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,17 @@
+x = 9
+y = 3
+result = x + y
+# result = x - y
+# result = x * y
+# Exponents
+# result = x ** y
+# result = x / y
+# result = x // y // removes decimal division pt and gives integer part
+# result = x % y // gives remainder that is modulus of it
+print(result)
+
+# Order of Operations
+# B E D M A S - Expression(E)
+
+num =input('Number: ')
+print(float(num) - 5)
