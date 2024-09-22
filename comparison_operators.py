6a0b0d9bba8a25fb6a86893662a1bdb7a6614c52
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-22 17:24:21 +0530

    comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,15 @@
+x = "hello"
+y = 'hello'
+
+print( x == y)
+print( x != y)
+print( x <= y)
+print( x >= y)
+print( x > y)
+print( x < y)
+
+#  now doing for ASCII characters
+
+print( 'a' > 'Z')
+# ord method checks ASCII value of character
+print(ord('a'))
